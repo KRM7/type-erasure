080c94dbafa799efbdb092ab33fbad5d3a58085e
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-11-19 17:54:31 +0100

    cleanup any-safe impl

--- any-safe/any.hpp
+++ any-safe/any.hpp
@@ -14,21 +14,22 @@ namespace any
         struct AnyImplBase
         {
             virtual std::unique_ptr<AnyImplBase> clone() const = 0;
-            virtual const std::type_info& type() const = 0;
+            virtual const std::type_info& type() const noexcept = 0;
             virtual ~AnyImplBase() = default;
         };
 
         template<typename T>
         struct AnyImpl : public AnyImplBase
         {
-            AnyImpl(T data) : data_(std::move(data)) {}
+            constexpr AnyImpl(T data) noexcept(std::is_nothrow_move_constructible_v<T>) :
+                data_(std::move(data)) {}
 
             std::unique_ptr<AnyImplBase> clone() const override
             {
                 return std::make_unique<AnyImpl>(data_);
             }
 
-            const std::type_info& type() const override
+            const std::type_info& type() const noexcept override
             {
                 return typeid(T);
             }
@@ -40,27 +41,27 @@ namespace any
     class Any
     {
     public:
-        Any() = default;
+        constexpr Any() noexcept :
+            data_(nullptr)
+        {}
 
         template<typename T>
-        requires (!std::is_same_v<std::remove_reference_t<T>, Any> && !std::is_array_v<std::remove_reference_t<T>>)
-        Any(T&& data) : data_(std::make_unique<detail::AnyImpl<std::remove_reference_t<T>>>(std::forward<T>(data)))
+        requires (!std::is_same_v<std::remove_cvref_t<T>, Any>) // This isnt the copy/move ctor
+        Any(T&& data) :
+            data_(std::make_unique<detail::AnyImpl<std::remove_cvref_t<T>>>(std::forward<T>(data)))
         {}
 
-        Any(const Any& other) : data_(other.data_->clone())
+        Any(const Any& other) :
+            data_(other.data_->clone())
         {}
 
-        Any& operator=(const Any& rhs)
+        Any(Any&&) = default;
+
+        Any& operator=(Any rhs) noexcept
         {
-            if (this == &rhs) return *this;
-
-            Any temp(rhs);
-            swap(temp);
+            swap(rhs);
             return *this;
         }
-
-        Any(Any&&)            = default;
-        Any& operator=(Any&&) = default;
 
         bool has_value() const noexcept
         { 
@@ -69,12 +70,12 @@ namespace any
 
         const std::type_info& type() const noexcept
         {
-            return has_value() ? data_->type() : typeid(void);
+            return data_ ? data_->type() : typeid(void);
         }
 
         void reset() noexcept
         {
-            data_.reset(nullptr);
+            data_ = nullptr;
         }
 
         void swap(Any& other) noexcept
@@ -85,15 +86,15 @@ namespace any
         template<typename T>
         T get()
         {
-            if (dynamic_cast<detail::AnyImpl<T>*>(data_.get()))
+            if (data_ && dynamic_cast<detail::AnyImpl<T>*>(data_.get()))
             {
                 return static_cast<detail::AnyImpl<T>*>(data_.get())->data_;
             }
-            throw std::logic_error("Bad cast");
+            throw std::logic_error("Bad Any cast");
         }
 
     private:
-        std::unique_ptr<detail::AnyImplBase> data_ = nullptr;
+        std::unique_ptr<detail::AnyImplBase> data_;
     };
 }
 
